f6a490f7e57e4c52255ca8227498f36dcc1f91a8
hebamohammed5590-sudo <hebamohammed5590@gmail.com>
2026-08-26 09:05:33 +0300

    fix: translate customer creation form

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -1,4 +1,10 @@
 <x-contact-form
     resource="customers"
     title="Add Customer"
-/>
+>
+    <div class="mt-4">
+        <label class="block font-medium text-sm text-gray-700" for="notes">
+            {{ __('Notes') }}
+        </label>
+    </div>
+</x-contact-form>
