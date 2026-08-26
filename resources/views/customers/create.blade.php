566c302020bd0569d385602475d56bbef695df4c
hebamohammed5590-sudo <hebamohammed5590@gmail.com>
2026-08-26 09:09:12 +0300

    fix: remove duplicate customer notes label

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -1,10 +1,4 @@
 <x-contact-form
     resource="customers"
     title="Add Customer"
->
-    <div class="mt-4">
-        <label class="block font-medium text-sm text-gray-700" for="notes">
-            {{ __('Notes') }}
-        </label>
-    </div>
-</x-contact-form>
+/>
